c96f4c231708dbb3a5ead876c5e8cba69f931858
TheOneBailey <121973831+TheOnlyBailey@users.noreply.github.com>
2025-04-28 22:07:01 +0100

    Update Solution.cpp

--- Social Network/Solution.cpp
+++ Social Network/Solution.cpp
@@ -10,7 +10,10 @@ bool Solution::processCommand(const std::string& commandString) {
 	std::string command;
 	inString >> command;
 
-	// Add your code here
+	//if (command == "ViewProfile")
+	//{
+	//	std::string identifier;
+	//}
 
 	return false;
 }
@@ -21,8 +24,18 @@ bool Solution::buildNetwork(const std::string& fileNameUsers, const std::string&
 	if (finUsers.fail() || finFriendships.fail()) {
 		return false;
 	}
+	std::string line;
+	while (std::getline(finUsers, line))
+	{
+		int start = 0;
+		std::string data[5];
+		for (int i = 0; start < line.length(); i++)
+		{
+			data[i] = line.substr(start, line.find(','));
+			start = line.substr(start, line.find(',')).length() + 1;
+		}
 
-	// ****** Add your code here *******
+	}
 	
 	return true;
 }
